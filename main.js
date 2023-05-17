b89131410db638ad26555b4af711ac0e13fb207d
Fran Linde Blázquez <franlindebl@gmail.com>
2023-05-17 02:04:54 +0200

    Added message to register SW

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 // Registro del Service Worker para habilitar la funcionalidad de PWA
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js');
+  navigator.serviceWorker
+    .register('/service-worker.js')
+    .then((registration) => {
+      console.log('Service Worker registrado con éxito:', registration);
+    })
+    .catch((error) => {
+      console.log('Error al registrar el Service Worker:', error);
+    });
 }
 
 // Función para pintar los datos de los pilotos en la página
